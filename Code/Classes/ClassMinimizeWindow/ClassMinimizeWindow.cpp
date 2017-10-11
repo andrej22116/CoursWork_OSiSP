dbf3f5fcb43b9d3bee3a08928943fefaf7f766e0
Андрей <andrej22116@hotmail.com>
2017-10-11 14:56:19 +0300

    Rename class ButtonMaximize

---
+++ Code/Classes/ClassMinimizeWindow/ClassMinimizeWindow.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+#include "ClassMinimizeWindow.h"
+
+namespace explorer {
+
+}
---
+++ Code/Classes/ClassMinimizeWindow/ClassMinimizeWindow.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "..\AbstractWindow\ClassAbstractWindow.h"
+
+#ifndef _CLASS_MINIMIZE_WINDOW_H_
+#define _CLASS_MINIMIZE_WINDOW_H_
+
+namespace explorer {
+
+}
+
+#endif
