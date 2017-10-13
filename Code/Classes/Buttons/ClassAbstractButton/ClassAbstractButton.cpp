d0824800f507b5b1416ce3bdc08478bd463f65d2
Андрей <andrej22116@hotmail.com>
2017-10-13 20:19:47 +0300

    Add abstract class Button!

---
+++ Code/Classes/Buttons/ClassAbstractButton/ClassAbstractButton.cpp
@@ -0,0 +1,2 @@
+#include "stdafx.h"
+#include "ClassAbstractButton.h"
--- Code/Classes/Buttons/ClassAbstractButton/ClassAbstractButton.h
+++ Code/Classes/Buttons/ClassAbstractButton/ClassAbstractButton.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "..\..\AbstractWindow\ClassAbstractWindow.h"
+
+#ifndef _CLASS_ABSTRACT_BUTTON_H_
+#define _CLASS_ABSTRACT_BUTTON_H_
+
+namespace explorer {
+
+	class Button : public Window {
+	public:
+		Button();
+
+		void hoverHandler(HWND hWnd, WPARAM wParam, LPARAM lParam);
+		void resizeParentHandler(HWND hWnd, WPARAM wParam, LPARAM lParam);
+	};
+
+}
+
+#endif
