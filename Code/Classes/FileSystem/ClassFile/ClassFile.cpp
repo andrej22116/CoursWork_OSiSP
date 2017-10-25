29a7abb42f3743cc6e46a9f0af6378f98715df2d
Андрей <andrej22116@hotmail.com>
2017-10-25 22:38:59 +0300

    Upp

--- Code/Classes/FileSystem/ClassFile/ClassFile.cpp
+++ Code/Classes/FileSystem/ClassFile/ClassFile.cpp
@@ -10,29 +10,29 @@ namespace explorer {
 
 	std::wstring File::getPath()
 	{
-
+		return L"";
 	}
 	std::wstring File::getName()
 	{
-
+		return L"";
 	}
 
 	bool File::isFile()
 	{
-
+		return true;
 	}
 	bool File::isDirectory()
 	{
-
+		return false;
 	}
 
 	std::vector<std::wstring> File::list()
 	{
-
+		return std::vector<std::wstring>();
 	}
 	std::vector<std::wstring> File::list(std::wstring filter)
 	{
-
+		return std::vector<std::wstring>();
 	}
 
 }
