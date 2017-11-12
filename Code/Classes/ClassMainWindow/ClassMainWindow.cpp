1b1d160d611907e84586bb4f791d1900afad3cf9
Andrew Stenyaew <andrej22116@hotmail.com>
2017-11-13 02:29:20 +0300

    Edit

--- Code/Classes/ClassMainWindow/ClassMainWindow.cpp
+++ Code/Classes/ClassMainWindow/ClassMainWindow.cpp
@@ -82,7 +82,7 @@ namespace explorer {
 					AccentPolicy policy = { 0, 0, 0, 229 };
 					policy.AccentFlags = 2;
 					policy.AccentState = 3;
-					policy.GradientColor = 0x84ffffff;
+					policy.GradientColor = 0x44ffffff;
 
 					data.attribute = 19;
 					data.pData = &policy;
